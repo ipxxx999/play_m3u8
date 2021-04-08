ee83c1b2904a38fbf34686775ffd7b169f0c56f6
ipxxx999 <59322632+ipxxx999@users.noreply.github.com>
2021-04-08 02:57:30 -0400

    Update file_get.php

--- file_get.php
+++ file_get.php
@@ -1,4 +1,5 @@
      <?php
+     // esta pagina te permitira copiar el codigo m3u8 o una pagina web
      //pagina web aqui
      // https://es.stackoverflow.com/questions/379571/file-get-contents-ssl-operation-failed-with-code-1-openssl-error-messages-e
 
@@ -11,7 +12,7 @@
         ),
     );  
     
-    $response = file_get_contents("https://live.spliktv.xyz/", false, stream_context_create($arrContextOptions));
+    $response = file_get_contents("https://github.com/ipxxx999/play_m3u8_1/blob/main/historia.m3u8", false, stream_context_create($arrContextOptions));
     
     echo $response;
 
